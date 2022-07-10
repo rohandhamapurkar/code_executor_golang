e0ccadfcdf648768b466b4ad3895dcd05e92f4eb
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2022-07-10 13:01:10 +0530

    wip: linux cmd execution

--- libs/exec/build.sh
+++ libs/exec/build.sh
@@ -1,55 +1,47 @@
 # install languages binaries
 
-
-
+mkdir -p /tmp/pkg/
+mkdir -p /tmp/
 
 # install NodeJS v14.20.0
-build_path=/usr/local/lib/node-v14.20.0-linux-x64
-pkg_file=node-v14.20.0-linux-x64.tar.xz
-mkdir -p $build_path
-mkdir -p /tmp/tar_pkgs/
-curl "https://nodejs.org/dist/v14.20.0/$pkg_file" -o /tmp/tar_pkgs/$pkg_file
-tar xf /tmp/tar_pkgs/$pkg_file --strip-components=1 --directory=$build_path
-rm /tmp/tar_pkgs/$pkg_file
-export PATH=$build_path/bin:$PATH
-echo "export PATH=$build_path/bin:$PATH" >> ~/.bashrc
+BUILD_FOLDER_NAME=node_v14.20.0
+PKG_NAME=node-v14.20.0-linux-x64.tar.xz
+PKG_SOURCE=https://nodejs.org/dist/v14.20.0/$PKG_NAME
+mkdir -p /tmp/pkg/$BUILD_FOLDER_NAME
+wget $PKG_SOURCE -P /tmp
+tar xf /tmp/$PKG_NAME --strip-components=1 --directory=/tmp/pkg/$BUILD_FOLDER_NAME
+rm /tmp/$PKG_NAME
+echo "PATH=/tmp/pkg/$BUILD_FOLDER_NAME/bin:$PATH" >> /tmp/pkg/$BUILD_FOLDER_NAME/.env
 
 
 
 # install Python 3.10.0
-# build_path=/usr/local/lib/Python-3.10.0
-# pkg_file=Python-3.10.0.tgz
-# mkdir -p $build_path
-# mkdir -p /tmp/tar_pkgs/
-# curl "https://www.python.org/ftp/python/3.10.0/$pkg_file" -o /tmp/tar_pkgs/$pkg_file
-# tar xzf /tmp/tar_pkgs/$pkg_file --strip-components=1 --directory=$build_path
-# rm /tmp/tar_pkgs/$pkg_file
-# cd $build_path
-# PREFIX=$(realpath $(dirname .))
-# ./configure --prefix "$PREFIX" --with-ensurepip=install
-# make -j$(nproc)
-# make install -j$(nproc)
-# rm -rf build
-# export PATH=$build_path/bin:$PATH
-# echo "export PATH=$build_path/bin:$PATH" >> ~/.bashrc
-# pip3 install numpy scipy pandas bcrypt
-
+BUILD_FOLDER_NAME=python_v3.10.0
+PKG_NAME=Python-3.10.0.tgz
+PKG_SOURCE=https://www.python.org/ftp/python/3.10.0/$PKG_NAME
+mkdir -p /tmp/pkg/$BUILD_FOLDER_NAME
+wget $PKG_SOURCE -P /tmp
+tar xzf /tmp/$PKG_NAME --strip-components=1 --directory=/tmp/pkg/$BUILD_FOLDER_NAME
+rm /tmp/$PKG_NAME
+cd /tmp/pkg/$BUILD_FOLDER_NAME
+PREFIX=$(realpath $(dirname .))
+./configure --prefix "$PREFIX" --with-ensurepip=install
+make -j$(nproc)
+make install -j$(nproc)
+rm -rf build
+echo "PATH=/tmp/pkg/$BUILD_FOLDER_NAME/bin:$PATH" >> /tmp/pkg/$BUILD_FOLDER_NAME/.env
+bin/pip3 install numpy scipy pandas bcrypt
+cd /tmp
 
 
 
 # install Java 15.0.2
-build_path=/usr/local/lib/openjdk-15.0.2_linux-x64_bin
-pkg_file=openjdk-15.0.2_linux-x64_bin.tar.gz
-mkdir -p $build_path
-mkdir -p /tmp/tar_pkgs/
-curl "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/$pkg_file" -o /tmp/tar_pkgs/$pkg_file
-tar xzf /tmp/tar_pkgs/$pkg_file --strip-components=1 --directory=$build_path
-rm /tmp/tar_pkgs/$pkg_file
-export PATH=$build_path/bin:$PATH
-echo "export PATH=$build_path/bin:$PATH" >> ~/.bashrc
-
-
-
-
-# source ~/.bashrc
-. ~/.bashrc
+# build_path=/usr/local/lib/openjdk-15.0.2_linux-x64_bin
+# pkg_file=openjdk-15.0.2_linux-x64_bin.tar.gz
+# mkdir -p $build_path
+# mkdir -p /tmp/tar_pkgs/
+# curl "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/$pkg_file" -o /tmp/tar_pkgs/$pkg_file
+# tar xzf /tmp/tar_pkgs/$pkg_file --strip-components=1 --directory=$build_path
+# rm /tmp/tar_pkgs/$pkg_file
+# export PATH=$build_path/bin:$PATH
+# echo "export PATH=$build_path/bin:$PATH" >> ~/.bashrc
--- libs/exec/exec_linux.go
+++ libs/exec/exec_linux.go
@@ -8,27 +8,37 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"syscall"
 )
 
 func main() {
 
-	cmd := exec.Command("bash", "run.sh", "java", "--version")
+	cmd := exec.Command("whoami")
 
 	var out bytes.Buffer
 	var errOut bytes.Buffer
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.Dir = "/go/exec/"
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{
+			Uid: 1001,
+			Gid: 1001,
+		},
+		Pgid: 0,
+	}
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "PATH=/tmp/pkg/node_v14.20.0/bin:/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin")
 	cmd.Stdout = &out
 	cmd.Stderr = &errOut
 
 	err := cmd.Run()
 
 	if err != nil {
-		log.Fatal(err)
-		log.Fatal(errOut.String())
+		log.Println(err)
+		log.Fatalln(errOut.String())
 	}
 
-	fmt.Printf(out.String())
+	fmt.Println(out.String())
 
 }
