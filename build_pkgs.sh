ff24269656a0f439108b75eecbc8490685260e0f
rohandhamapurkar <dhamapurkar54@gmail.com>
2022-07-15 13:06:11 +0530

    feat: added python and golang pkg dep

--- build_pkgs.sh
+++ build_pkgs.sh
@@ -15,19 +15,29 @@ ls /pkg/node_v14.20.0
 
 
 # install Python 3.10.0
-# BUILD_FOLDER_NAME=python_v3.10.0
-# PKG_NAME=Python-3.10.0.tgz
-# PKG_SOURCE=https://www.python.org/ftp/python/3.10.0/$PKG_NAME
-# mkdir -p /pkg/$BUILD_FOLDER_NAME
-# wget $PKG_SOURCE -P /tmp
-# tar xzf /pkg/$PKG_NAME --strip-components=1 --directory=/pkg/$BUILD_FOLDER_NAME
-# rm /pkg/$PKG_NAME
-# cd /pkg/$BUILD_FOLDER_NAME
-# PREFIX=$(realpath $(dirname .))
-# ./configure --prefix "$PREFIX" --with-ensurepip=install
-# make -j$(nproc)
-# make install -j$(nproc)
-# rm -rf build
-# echo "PATH=/pkg/$BUILD_FOLDER_NAME/bin:$PATH" >> /pkg/$BUILD_FOLDER_NAME/.env
-# bin/pip3 install numpy scipy pandas bcrypt
-# cd /tmp
+BUILD_FOLDER_NAME=python_v3.10.0
+PKG_NAME=Python-3.10.0.tgz
+PKG_SOURCE=https://www.python.org/ftp/python/3.10.0/$PKG_NAME
+mkdir -p /pkg/$BUILD_FOLDER_NAME
+wget $PKG_SOURCE -P /pkg
+tar xzf /pkg/$PKG_NAME --strip-components=1 --directory=/pkg/$BUILD_FOLDER_NAME
+rm /pkg/$PKG_NAME
+cd /pkg/$BUILD_FOLDER_NAME
+PREFIX=$(realpath $(dirname .))
+./configure --prefix "$PREFIX" --with-ensurepip=install
+make -j$(nproc)
+make install -j$(nproc)
+rm -rf build
+echo "PATH=/pkg/$BUILD_FOLDER_NAME/bin:$PATH" >> /pkg/$BUILD_FOLDER_NAME/.env
+bin/pip3 install numpy scipy pandas bcrypt
+
+
+# install Golang 1.18.4
+BUILD_FOLDER_NAME=golang_v1.18.4
+PKG_NAME=go1.18.4.linux-amd64.tar.gz
+PKG_SOURCE=https://golang.org/dl/$PKG_NAME
+mkdir -p /pkg/$BUILD_FOLDER_NAME
+wget $PKG_SOURCE -P /pkg
+tar -xzf /pkg/$PKG_NAME --strip-components=1 --directory=/pkg/$BUILD_FOLDER_NAME
+rm /pkg/$PKG_NAME
+echo "PATH=/pkg/$BUILD_FOLDER_NAME/bin:$PATH" >> /pkg/$BUILD_FOLDER_NAME/.env
